ad10436a76920ab05e679ca8c8f96e62f1cef9ab
Ameena <ameenazara3@gmail.com>
2023-12-04 15:17:48 -0500

    updating App.js

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Amplify, API} from 'aws-amplify';
+import { Amplify, API } from 'aws-amplify';
 import amplifyconfig from './src/amplifyconfiguration.json';
+import React, { useEffect } from 'react';
+
 Amplify.configure(amplifyconfig);
 
 
 export default function App() {
-  async function fetchWoohoo(){
-      API
-      
-      .get("mealbuddylambda", "/items", {}).then((response) => {
-        setMyMessage(response.success);
-        console.log(`Response: ${response}`);
-      }).catch((error) => {
-        console.log(`Error: ${error}`);
-      });
-    }
+  useEffect(() => {
+    // Function to make the API call
+    const fetchDataFromApi = async () => {
+      try {
+        const apiResponse = await API.get('mealbuddyapi', '/items');
+        console.log('API Response:', apiResponse);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-    return (
-      <View style={styles.container}>
-        <Text>MealBuDDY!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
+    // Call the API function
+    fetchDataFromApi();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>MealBuDDY!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({

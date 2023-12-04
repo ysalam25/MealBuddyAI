9eb5ff2201d2bf429a9dadee72267d65198ce985
ysalam25 <ysalam@uwo.ca>
2023-12-03 22:07:54 -0500

    Initialized Backend

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>MealBuDDY!</Text>
       <StatusBar style="auto" />
     </View>
   );

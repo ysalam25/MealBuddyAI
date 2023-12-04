459e3725c92d755f3fed0a67da1402011d30c7ef
Ameena <ameenazara3@gmail.com>
2023-12-04 14:46:40 -0500

    adding api

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Amplify } from 'aws-amplify';
+import { Amplify, API} from 'aws-amplify';
 import amplifyconfig from './src/amplifyconfiguration.json';
 Amplify.configure(amplifyconfig);
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>MealBuDDY!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  async function fetchWoohoo(){
+      API
+      
+      .get("mealbuddylambda", "/items", {}).then((response) => {
+        setMyMessage(response.success);
+        console.log(`Response: ${response}`);
+      }).catch((error) => {
+        console.log(`Error: ${error}`);
+      });
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text>MealBuDDY!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({

babea23a0587246b630ea789007c7969365cbb27
ysalam25 <ysalam@uwo.ca>
2024-01-12 00:50:46 -0500

    Added filter over lay screen

--- pages/Walkthrough.js
+++ pages/Walkthrough.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-
+import { Modal, View, Text, TouchableOpacity } from "react-native";
 import {
   StyledContainer,
   InnerContainer,
@@ -16,13 +16,45 @@ import {
   ExtraText,
   TextLink,
   TextLinkContent,
+  OverlappingScreen,
+  OverlappingContent,
+  OverlappingText,
+  CloseButton,
+  CloseButtonText,
+  OverlappingTitle,
+  CategoryButton,
+  CategoryContainer,
+  CategoryButtonText,
+  ApplyContainer,
+  FilterButton,
+  CrossSVGIcon,
+  StarSVGIcon,
+  RatingContainer,
 } from "./../components/styles";
 
 import { Formik } from "formik";
-import { View } from "react-native";
 import { Colors } from "./../components/styles";
 
 const Walkthrough = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [starStates, setStarStates] = useState([false, false, false, false, false]);
+
+
+  const handleButtonClick = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleStarClick = (index) => {
+    setStarStates((prevStars) => {
+      const updatedStars = prevStars.map((_, i) => i <= index);
+      return updatedStars;
+    });
+  };
+  
   return (
     <StyledContainer>
       <StatusBar style="dark" />
@@ -36,23 +68,76 @@ const Walkthrough = () => {
               <TextLinkContent>Sign Up</TextLinkContent>
             </TextLink>
           </ExtraView>
-          <StyledButton>
+          <StyledButton onPress={handleButtonClick}>
             <ButtonText>YY</ButtonText>
           </StyledButton>
         </LoginContainer>
       </InnerContainer>
+
+      <Modal visible={showModal} animationType="slide" transparent={true}>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <OverlappingScreen>
+            <OverlappingContent>
+            <TouchableOpacity onPress={closeModal}>
+                <CrossSVGIcon />
+              </TouchableOpacity>
+              <OverlappingTitle>Category</OverlappingTitle>
+               <CategoryContainer>
+              <CategoryButton>
+                <CategoryButtonText>BreakFast</CategoryButtonText>
+              </CategoryButton>
+              <CategoryButton>
+                <CategoryButtonText>Lunch</CategoryButtonText>
+              </CategoryButton>
+              <CategoryButton>
+                <CategoryButtonText>Dinner</CategoryButtonText>
+              </CategoryButton>
+              <CategoryButton>
+                <CategoryButtonText>Snack</CategoryButtonText>
+              </CategoryButton>
+              </CategoryContainer>
+              <OverlappingTitle>Cuisine Type</OverlappingTitle>
+              <CategoryContainer>
+              <CategoryButton>
+                <CategoryButtonText>Mexican</CategoryButtonText>
+              </CategoryButton>
+              <CategoryButton>
+                <CategoryButtonText>Italian</CategoryButtonText>
+              </CategoryButton>
+              <CategoryButton>
+                <CategoryButtonText>Mediterranean</CategoryButtonText>
+              </CategoryButton>
+              <CategoryButton>
+                <CategoryButtonText>Asian Fusion</CategoryButtonText>
+              </CategoryButton>
+              <CategoryButton>
+                <CategoryButtonText>Indian</CategoryButtonText>
+              </CategoryButton>
+              <CategoryButton>
+                <CategoryButtonText>Comfort</CategoryButtonText>
+              </CategoryButton>
+              </CategoryContainer>
+             <OverlappingTitle>Rating</OverlappingTitle>
+             <RatingContainer>
+                {starStates.map((isFilled, index) => (
+                  <TouchableOpacity key={index} onPress={() => handleStarClick(index)}>
+                    <StarSVGIcon isFilled={isFilled} color='#9095A0' />
+                  </TouchableOpacity>
+                ))}
+              </RatingContainer>
+            <ApplyContainer>
+              <FilterButton>
+                <CategoryButtonText>Reset</CategoryButtonText>
+              </FilterButton>
+              <FilterButton>
+                <CategoryButtonText>Apply</CategoryButtonText>
+              </FilterButton>
+            </ApplyContainer>
+            </OverlappingContent>
+          </OverlappingScreen>
+        </View>
+      </Modal>
     </StyledContainer>
-  );
-};
-
-//Colors
-const { primary, darkLight } = Colors;
-
-const MyTextInput = ({ label, ...props }) => {
-  return (
-    <View>
-      <StyledTextInput {...props} />
-    </View>
   );
 };
 

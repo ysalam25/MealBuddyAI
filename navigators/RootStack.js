65e036cdb226ca8d8131295b3d4cbca77df0cb2f
ysalam25 <ysalam@uwo.ca>
2024-01-12 22:51:07 -0500

    more changes from ameena

--- navigators/RootStack.js
+++ navigators/RootStack.js
@@ -7,15 +7,12 @@ const { primary, secondary } = Colors;
 //pages
 import Login from "./../pages/Login";
 import Signup from "./../pages/Signup";
-import ConfirmEmail from "./../pages/ConfirmEmail";
 import ForgotPassword from "./../pages/ForgotPassword";
 import NewPasswordScreen from "./../pages/NewPasswordScreen";
 import Walkthrough1 from "./../pages/Walkthrough1";
 import Settings from "./../pages/Settings";
 import Tabs from "../navigators/tabs"; 
 import ConfirmEmail from "./../pages/ConfirmEmail";
-import ForgotPassword from "./../pages/ForgotPassword";
-import NewPasswordScreen from "./../pages/NewPasswordScreen";
 import DietaryPreferences from "./../pages/DietaryPreferences";
 import LoginNewUser from "./../pages/LoginNewUser";
 
@@ -58,11 +55,8 @@ const RootStack = () => {
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       <Stack.Screen name="NewPasswordScreen" component={NewPasswordScreen} />
       <Stack.Screen name="Settings" component={Settings} />
-        <Stack.Screen name="ConfirmEmail" component={ConfirmEmail} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="NewPasswordScreen" component={NewPasswordScreen} />
-        <Stack.Screen name="DietaryPreferences" component={DietaryPreferences} />
-        <Stack.Screen name="LoginNewUser" component={LoginNewUser} />
+      <Stack.Screen name="DietaryPreferences" component={DietaryPreferences} />
+      <Stack.Screen name="LoginNewUser" component={LoginNewUser} />
         
     </Stack.Navigator>
   );
